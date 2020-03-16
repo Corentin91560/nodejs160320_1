6f604ebf4eb1aca8bb5770a4d8d98dc58aca2dbf
corentin delage <corentin.delage@hotmail.fr>
2020-03-16 14:59:43 +0100

    mise a jour exo3

--- server.js
+++ server.js
@@ -2,8 +2,14 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/hello', function (req, res) {
+
+    if (req.query.nom) {
+      res.send('Bonjour,'+req.query.nom);
+    }else {
+      res.send('Quel est votre nom ?');
+    }
+
 })
 
 app.listen(process.env.PORT || 3000, function () {
